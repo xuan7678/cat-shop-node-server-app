0b559b9df199f6f8421938b006f3ae340012b366
feiergu <gu.fei@northeastern.edu>
2023-11-30 09:52:41 -0800

    Complete upload image API

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
+import path from "path";
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import ProductRoutes from "./products/routes.js";
 import OrderRoutes from "./orders/routes.js";
+import UploadRoutes from "./upload/routes.js";
 
 mongoose.connect("mongodb://localhost:27017/catShop");
 const app = express();
@@ -10,4 +12,16 @@ app.use(cors());
 app.use(express.json());
 ProductRoutes(app);
 OrderRoutes(app);
+UploadRoutes(app);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static("/var/data/uploads"));
+} else {
+  const __dirname = path.resolve();
+  app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
 app.listen(process.env.PORT || 4000);

7c206b75dd44a44cc84ebbbadc29ec5df433b685
xuan7678 <xuanxuan2980@gmail.com>
2023-11-29 20:51:11 -0800

    user apis

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import userRoutes from './users/userRoutes.js';
 
-mongoose.connect("mongodb://localhost:27017/catShop");
+mongoose.connect("mongodb://127.0.0.1:27017/catShop");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use('/api/users', userRoutes);
 app.listen(process.env.PORT || 4000);

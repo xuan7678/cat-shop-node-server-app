19a65cee7b99c36a834eb14308ee467c8c067eff
xuan7678 <xuanxuan2980@gmail.com>
2023-12-04 21:00:09 -0800

    add api for review

--- reviews/reviewRoutes.js
+++ reviews/reviewRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import {admin, protect} from "../middleware/authMiddleware.js";
-import { getReviews, createReview } from "./reviewController.js";
+import { getReviews, createReview, getReviewsByProductId } from "./reviewController.js";
 
 const router = express.Router();
 
 router.route('/').get(protect, admin, getReviews)
     .post(protect, createReview);
+
+router.route('/:productId').get(getReviewsByProductId)
 
 export default router;

e5657d1beb3c2a14e3f75226f897907a6cc0ad6c
xuan7678 <xuanxuan2980@gmail.com>
2023-12-12 09:30:13 -0800

    Revert "allow token"
    This reverts commit e9c311cfe5d0292e0d95524aa26317c5f8d4ed8e.

--- users/generateToken.js
+++ users/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    domain: process.env.BACK_END_DOMAIN || 'localhost',
+    sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };

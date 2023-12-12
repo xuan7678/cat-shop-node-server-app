1c12cf6a11ab11af92ab4a42da53a390a9e3a189
xuan7678 <xuanxuan2980@gmail.com>
2023-12-12 09:29:54 -0800

    Revert "token"
    This reverts commit 877efb3e751334d38e434872dcfd5415bcb2fd8c.

--- users/generateToken.js
+++ users/generateToken.js
@@ -8,6 +8,7 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     domain: process.env.BACK_END_DOMAIN || 'localhost',
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
